5e25437c441c1cdd8ffe09cdda8de25f3519d420
Vlad Khomenko <theromanovich03@gmail.com>
2023-11-30 14:23:46 +0200

    finish script

--- script.mjs
+++ script.mjs
@@ -17,14 +17,39 @@ if (!fs.existsSync(outputFolderPath)) {
 async function translateFile(filePath, targetLanguage) {
   const content = fs.readFileSync(filePath, 'utf-8');
 
+  // Находим блоки `--- ---` и сохраняем их без изменений
   const frontMatterRegex = /^---([\s\S]*?)---/;
-  const contentWithoutFrontMatter = content.replace(frontMatterRegex, '');
+  const frontMatterMatch = content.match(frontMatterRegex);
+  const frontMatter = frontMatterMatch ? frontMatterMatch[0] : '';
+
+  // Исключаем блок `--- ---` из контента для перевода
+  const contentToTranslate = content.replace(frontMatter, '');
 
   try {
-    const response = await translator.translateText(contentWithoutFrontMatter, null, targetLanguage);
+    // Разделяем ключи и значения более точным образом
+    const frontMatterLines = frontMatter.split('\n');
+    const translatedFrontMatter = await Promise.all(frontMatterLines.map(async (line) => {
+      if (line.trim() === '' || line.includes(':')) {
+        const key = line.split(':')[0].trim();
+        const value = line.slice(line.indexOf(':') + 1).trim();
+        if (value !== '') {
+          const translation = await translator.translateText(value, null, targetLanguage);
+          return `${key}: ${translation.text}`;
+        } else {
+          return line;
+        }
+      } else {
+        // Строки без ':' остаются без изменений
+        return line;
+      }
+    }));
+
+    // Переводим только контент без блока `--- ---`
+    const response = await translator.translateText(contentToTranslate, null, targetLanguage);
     console.log(response.text);
 
-    const translatedContent = content.replace(contentWithoutFrontMatter, response.text);
+    // Восстанавливаем оригинальные ключи и переведенный контент
+    const translatedContent = translatedFrontMatter.join('\n') + response.text;
     const outputFilePath = path.join(outputFolderPath, path.basename(filePath));
     fs.writeFileSync(outputFilePath, translatedContent, 'utf-8');
 
@@ -33,6 +58,7 @@ async function translateFile(filePath, targetLanguage) {
   }
 }
 
+// Получаем язык из аргументов командной строки или используем значение по умолчанию ('RU')
 const targetLanguage = process.argv[2] || 'RU';
 
 fs.readdirSync(inputFolderPath)
