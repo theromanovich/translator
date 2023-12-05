33ef1e85f80d6d8726ca6ebd69217ca062ebf6ab
Vlad Khomenko <theromanovich03@gmail.com>
2023-12-05 14:11:40 +0200

    update

--- script.mjs
+++ script.mjs
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
 import * as deepl from 'deepl-node';
 
@@ -10,22 +10,18 @@ const translator = new deepl.Translator(authKey);
 const inputFolderPath = path.join(__dirname, 'pages');
 const outputFolderPath = path.join(__dirname, 'translated_pages');
 
-if (!fs.existsSync(outputFolderPath)) {
-  fs.mkdirSync(outputFolderPath);
-}
-
-async function translateFile(filePath, targetLanguage) {
-  const content = fs.readFileSync(filePath, 'utf-8');
-
-  const frontMatterRegex = /^---([\s\S]*?)---/;
-  const frontMatterMatch = content.match(frontMatterRegex);
-  const frontMatter = frontMatterMatch ? frontMatterMatch[0] : '';
-
-  const contentToTranslate = content.replace(frontMatter, '');
-
-  const staticFrontMatterKeys = ['layout', 'permalink', 'ru_permalink']
-
+async function translateFile(inputFilePath, targetLanguage, outputFolderPath) {
   try {
+    const content = await fs.readFile(inputFilePath, 'utf-8');
+
+    const frontMatterRegex = /^---([\s\S]*?)---/;
+    const frontMatterMatch = content.match(frontMatterRegex);
+    const frontMatter = frontMatterMatch ? frontMatterMatch[0] : '';
+
+    const contentToTranslate = content.replace(frontMatter, '');
+
+    const staticFrontMatterKeys = ['layout', 'permalink', 'ru_permalink'];
+
     const frontMatterLines = frontMatter.split('\n');
     const translatedFrontMatter = await Promise.all(frontMatterLines.map(async (line) => {
       if (line.trim() === '' || line.includes(':')) {
@@ -46,19 +42,42 @@ async function translateFile(filePath, targetLanguage) {
     const response = await translator.translateText(contentToTranslate, null, targetLanguage);
 
     const translatedContent = translatedFrontMatter.join('\n') + response.text;
-    const outputFilePath = path.join(outputFolderPath, path.basename(filePath));
-    fs.writeFileSync(outputFilePath, translatedContent, 'utf-8');
+    
+    // Определяем относительный путь и создаем соответствующую папку в outputFolderPath
+    const relativePath = path.relative(inputFolderPath, inputFilePath);
+    const outputFilePath = path.join(outputFolderPath, relativePath);
+
+    await fs.mkdir(path.dirname(outputFilePath), { recursive: true });
+    await fs.writeFile(outputFilePath, translatedContent, 'utf-8');
 
   } catch (error) {
-    console.error(`Error translating ${filePath}: ${error.message}`);
+    console.error(`Error translating ${inputFilePath}: ${error.message}`);
   }
 }
 
-const targetLanguage = process.argv[2] || 'RU';
+async function translateFilesRecursively(inputFolderPath, outputFolderPath, targetLanguage) {
+  try {
+    const items = await fs.readdir(inputFolderPath);
 
-fs.readdirSync(inputFolderPath)
-  .filter((file) => file.endsWith('.md'))
-  .forEach((file) => {
-    const filePath = path.join(inputFolderPath, file);
-    translateFile(filePath, targetLanguage);
-});
+    for (const item of items) {
+      const inputItemPath = path.join(inputFolderPath, item);
+
+      const stats = await fs.stat(inputItemPath);
+
+      if (stats.isDirectory()) {
+        await translateFilesRecursively(inputItemPath, outputFolderPath, targetLanguage);
+      } else if (stats.isFile() && item.endsWith('.md')) {
+        await translateFile(inputItemPath, targetLanguage, outputFolderPath);
+      }
+    }
+  } catch (error) {
+    console.error(`Error reading directory ${inputFolderPath}: ${error.message}`);
+  }
+}
+
+try {
+  const targetLanguage = process.argv[2] || 'RU';
+  await translateFilesRecursively(inputFolderPath, outputFolderPath, targetLanguage);
+} catch (error) {
+  console.error(`Error translating files: ${error.message}`);
+}
